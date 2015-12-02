4321f42742604c0f59049489237b5e4fad51a8d4
Christopher Hiller <boneskull@boneskull.com>
2015-12-02 02:53:10 -0800

    remove unintended crap in debuggable

--- src/core/debuggable.js
+++ src/core/debuggable.js
@@ -6,19 +6,11 @@ const packageName = require('../../package.json').name;
 const Debuggable = stampit({
   'static': {
     namespace(ns = packageName) {
-
-      return this.init(function debuggableInit() {
-        privateState.set(this, {
-
-        })
-      })
-        .methods({
+      return this.methods({
         debug: require('debug')(ns)
       });
     }
   }
 });
-
-const privateState = require('./private-state')(Debuggable);
 
 module.exports = Debuggable;
