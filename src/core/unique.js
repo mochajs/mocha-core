75827a2f9a48808bc7a50e4aacb5c0f0db0f358a
Christopher Hiller <boneskull@boneskull.com>
2015-12-08 22:59:38 -0800

    no need for idProp

--- src/core/unique.js
+++ src/core/unique.js
@@ -2,13 +2,10 @@
 
 const stampit = require('stampit');
 
-const idProp = Symbol();
-
 const Unique = stampit({
   init() {
-    this[idProp] = Symbol();
     Object.defineProperty(this, 'id', {
-      value: this[idProp],
+      value: Symbol(),
       writable: false,
       configurable: true
     });
