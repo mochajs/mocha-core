82557f6d60db231edde5e371c2ba07b872624e5e
Christopher Hiller <boneskull@boneskull.com>
2015-12-22 05:02:08 -0800

    update mocha.js to use Pluggable

--- src/mocha.js
+++ src/mocha.js
@@ -1,40 +1,26 @@
 'use strict';
 
 const stampit = require('stampit');
-const API = require('./core/api');
+const Pluggable = require('./core/pluggable');
 const UI = require('./ui');
 const Reporter = require('./reporter');
-const debug = require('debug')('mocha:mocha');
 
 const Mocha = stampit({
   refs: {
     ui: require('mocha-ui-bdd')
   },
   methods: {
-    start() {
-
-    },
     run() {
-      this.emit('pre-run');
-      return this.loadPlugins()
-        .then(plugins => {
-          debug(`Loaded plugins ${plugins}`);
-          this.emit('ready');
-        })
-        .return(this);
     },
     createUI() {
       return UI({mocha: this});
     },
     createReporter() {
       return Reporter({mocha: this});
-    },
-    createAPI() {
-      return API({mocha: this});
     }
   }
 })
-  .compose(API)
+  .compose(Pluggable)
   .init(function initMocha({stamp}) {
     this.use(this.ui);
     this.Mocha = stamp;
