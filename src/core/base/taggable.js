6ab6a55506810a97d7e2962b2136b2008e304415
Christopher Hiller <boneskull@boneskull.com>
2015-12-23 21:13:08 -0800

    add Taggable and spec

---
+++ src/core/base/taggable.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const stampit = require('stampit');
+const _ = require('lodash');
+
+function tag(...tags) {
+  const tagSet = new Set();
+  _.forEach(tags, tag => tagSet.add(tag));
+  return this.init(function tagInit({instance}) {
+    _.forEach(instance.tags, tag => tagSet.add(tag));
+    this.tags = tagSet;
+  });
+}
+
+const Taggable = stampit({
+  static: {
+    tag,
+    tags: tag
+  }
+});
+
+module.exports = Taggable;
---
+++ test/unit/core/base/taggable.spec.js
@@ -0,0 +1,41 @@
+'use strict';
+
+describe(`core/base/taggable`, () => {
+  const Taggable = require('../../../../src/core/base/taggable');
+
+  describe(`Taggable()`, () => {
+    describe(`static method`, () => {
+      describe(`tag()`, () => {
+        it(`should cause the instantiated object to have a Set "tags" prop`,
+          () => {
+            const obj = Taggable.tag('foo', 'bar')();
+            expect(obj.tags)
+              .to
+              .be
+              .a('Set');
+          });
+
+        it(`should each each argument to the instantiated object's "tags" prop`,
+          () => {
+            const obj = Taggable.tag('foo', 'bar')();
+            expect(obj.tags.has('foo')).to.be.true;
+            expect(obj.tags.has('bar')).to.be.true;
+          });
+
+        it(`should merge any tags in the instance`, () => {
+          const obj = Taggable.tag('foo', 'bar')({tags: ['baz']});
+          expect(obj.tags.has('foo')).to.be.true;
+          expect(obj.tags.has('bar')).to.be.true;
+        });
+      });
+
+      describe(`tags()`, () => {
+        it(`should alias tag()`, () => {
+          expect(Taggable.tag)
+            .to
+            .equal(Taggable.tags);
+        });
+      });
+    });
+  });
+});
