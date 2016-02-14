c1ad2ee6a31f36c413114a4ede764b5f34faa364
Christopher Hiller <boneskull@boneskull.com>
2016-02-13 22:27:06 -0800

    better karma setup, but probably still broken

--- .karma.conf.js
+++ .karma.conf.js
@@ -1,26 +1,36 @@
 'use strict';
 
+var pkg = require('./package.json');
+var globby = require('globby');
+var _ = require('lodash');
+var path = require('path');
+
 module.exports = function(config) {
   config.set({
     frameworks: [
       'mocha',
       'browserify'
     ],
-    files: [
-      require.resolve('phantomjs-polyfill'),
-      'src/**/*.js',
-      'test/unit/**/*.js'
-    ],
+    files: globby.sync([
+      'node_modules/babel-polyfill/dist/polyfill.min.js',
+      './src/**/*.js',
+      './test/unit/**/*.js',
+      '!./src/util/async-listener.js',
+      '!./src/plugins/resolver.js',
+      '!./src/options/rc.js'
+    ]),
     preprocessors: {
-      'src/**/*.js': 'browserify',
-      'test/unit/**/*.js': 'browserify'
+      './src/**/*.js': ['browserify'],
+      './test/**/*.js': ['browserify']
     },
     browserify: {
-      debug: true
+      debug: true,
+      transform: pkg.browserify.transform
     },
     reporters: [
       'mocha'
     ],
+    logLevel: 'WARN',
     autoWatch: true,
     browsers: ['PhantomJS'],
     singleRun: false
