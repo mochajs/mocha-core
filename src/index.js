59b206173d2005b5ac3e95fb4d0247dd164cefbb
Christopher Hiller <boneskull@boneskull.com>
2016-03-10 21:57:31 -0800

    remove dumb jsdoc from index.js; tweak exports

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
 'use strict';
 
-/**
- * Mocha's main entry point.  Loads shims/globals (async-listener/zone.js
- * depending on context)
- * @module index
- * @see https://www.npmjs.com/package/source-map-support
- * @see https://www.npmjs.com/package/async-listener
- * @see https://www.npmjs.com/package/zone.js
- */
-
 import 'source-map-support/register';
 import 'babel-polyfill';
 import Mocha from './mocha';
 import pkg from './options/package';
 import './util/mixins';
 
-/**
- * Default instance of Mocha.  It doesn't *need* to be used by consumers, but
- * exists for the sake of convenience.
- * @type {Mocha}
- */
-export const mocha = Mocha();
-
-export const version = pkg.version;
+const mocha = Mocha();
+const version = pkg.version;
+export {Mocha, version, mocha as default};
