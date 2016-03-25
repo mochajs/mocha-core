f17ff937650b1d27dc5b7229d131faea89e10198
Christopher Hiller <boneskull@boneskull.com>
2016-03-24 22:01:38 -0700

    add isPromise mixin

--- src/util/mixins.js
+++ src/util/mixins.js
@@ -9,3 +9,9 @@ is.mixin(isNull, 'null');
 is.mixin(function isArrayOfUnemptyStrings (value) {
   return is.arrayOf(is.unemptyString, value);
 }, 'arrayOfUnemptyStrings');
+
+const isPromise = is.schema({
+  then: is.function
+});
+
+is.mixin(isPromise, 'promise');
