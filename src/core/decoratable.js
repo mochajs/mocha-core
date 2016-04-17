dac77e3c63d2e6d4542c48f1e2fbd34b30db3285
Christopher Hiller <boneskull@boneskull.com>
2016-04-17 16:44:29 -0700

    do not require a function when decorating

--- src/core/decoratable.js
+++ src/core/decoratable.js
@@ -8,24 +8,21 @@ const Decoratable = stampit({
     delegate: {}
   },
   methods: {
-    decorate (name, func, opts = {}) {
-      if (is.array(name)) {
-        forEach(name,
+    decorate (propName, propValue, opts = {}) {
+      if (is.array(propName)) {
+        forEach(propName,
           value => this.decorate(value.name, value.func, value.opts));
         return this;
-      } else if (is.object(name)) {
-        forEach(name, (value, key) => this.decorate(key, value));
+      } else if (is.object(propName)) {
+        forEach(propName, (value, key) => this.decorate(key, value));
         return this;
-      }
-      if (is.not.function(func)) {
-        throw new Error('"func" must be a Function');
       }
       opts = defaults({
         args: [],
         context: this
       }, opts);
       // TODO: given an option, warn the user if a plugin blasts an existing plugin
-      this.delegate[name] = func.bind(opts.context, ...opts.args);
+      this.delegate[propName] = propValue.bind(opts.context, ...opts.args);
       return this;
     }
   }
