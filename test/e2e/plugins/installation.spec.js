5158c09a105ecd1c6104eaf597a429ec0b630f02
Christopher Hiller <boneskull@boneskull.com>
2016-04-23 23:59:12 -0700

    update E2E plugin installation test

--- test/e2e/plugins/installation.spec.js
+++ test/e2e/plugins/installation.spec.js
@@ -15,7 +15,6 @@ describe('e2e/plugins/installation', () => {
     describe('when the Plugin is ready', () => {
       it('should install the plugin', () => {
         pluggable.use(plugin);
-        pluggable.ready();
         expect(pluggable.loadedPlugins.get(plugin.attributes.name))
           .to
           .be
@@ -24,6 +23,13 @@ describe('e2e/plugins/installation', () => {
           .to
           .have
           .property('name', 'foo');
+      });
+
+      it('should set "ready" to true', () => {
+        expect(pluggable)
+          .to
+          .have
+          .property('ready', true);
       });
     });
   });
@@ -56,18 +62,29 @@ describe('e2e/plugins/installation', () => {
     });
 
     describe('and when the deps are not installed', () => {
-      it('should not immediately install the plugin', () => {
+      beforeEach(() => {
         pluggable.use(plugin);
-        expect(pluggable.loadedPlugins.size)
+      });
+
+      it('should not immediately install the plugin', () => {
+        expect(pluggable)
           .to
-          .equal(0);
+          .have
+          .deep
+          .property('loadedPlugins.size', 0);
+      });
+
+      it('should set "ready" to be false', () => {
+        expect(pluggable)
+          .to
+          .have
+          .property('ready', false);
       });
 
       describe('and the Pluggable is ready', () => {
         it('should emit "error"', () => {
           expect(() => {
             pluggable.use(plugin);
-            pluggable.ready();
           })
             .to
             .emitFrom(pluggable, 'error');
@@ -76,36 +93,53 @@ describe('e2e/plugins/installation', () => {
     });
 
     describe('and when the deps are installed', () => {
-      it('should emit "done"', () => {
-        expect(() => {
-          pluggable.use(plugin);
-          pluggable.use(dep);
-          pluggable.ready();
-        })
-          .to
-          .emitFrom(pluggable, 'done');
-      });
-
-      it('should put the installed plugin into the "loadedPlugins" Mappable', () => {
+      beforeEach(() => {
         pluggable.use(plugin)
           .use(dep);
-        pluggable.ready();
-        expect(pluggable.loadedPlugins.get(plugin.attributes.name))
+      });
+
+      it('should set "ready" to true', () => {
+        expect(pluggable)
           .to
           .have
-          .property('name', plugin.attributes.name);
+          .property('ready', true);
       });
+
+      it('should put the installed plugin into the "loadedPlugins" Mappable',
+        () => {
+          expect(pluggable.loadedPlugins.get(plugin.attributes.name))
+            .to
+            .have
+            .property('name', plugin.attributes.name);
+        });
 
       it('should put the installed dependency into the "loadedPlugins" Mappable',
         () => {
-          pluggable.use(plugin)
-            .use(dep);
-          pluggable.ready();
           expect(pluggable.loadedPlugins.get(dep.attributes.name))
             .to
             .have
             .property('name', dep.attributes.name);
         });
+    });
+
+    describe('and the plugin throws upon install', () => {
+      let err;
+
+      beforeEach(() => {
+        err = new Error();
+        plugin = function myPlugin () {
+          throw err;
+        };
+        plugin.attributes = {
+          name: 'foo'
+        };
+      });
+
+      it('should emit "error"', () => {
+        expect(() => pluggable.use(plugin))
+          .to
+          .emitFrom(pluggable, 'error', err);
+      });
     });
   });
 });
